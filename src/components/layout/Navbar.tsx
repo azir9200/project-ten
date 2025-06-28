7bcdae3efc9a30fa2f7261b9461f7b85c87f5b92
Azir@9200 <aziruddin83@hotmail.com>
2025-06-28 20:29:34 +0100

    dark mode added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../modeToggle";
 
 const Navbar = () => {
   return (
     <nav className="max-w-7xl mx-auto flex items-center justify-center gap-10 px-8 py-12 bg-purple-500">
-      <Link className="bg-gray-400 px-6 py-2" to="/task">
-        Task
-      </Link>
-      <Link className="bg-gray-400 px-6 py-2" to="/user">
-        Users
-      </Link>
+      <div>
+        {" "}
+        <Link className="bg-gray-400 px-6 py-2" to="/task">
+          Task
+        </Link>
+        <Link className="bg-gray-400 px-6 py-2" to="/user">
+          Users
+        </Link>
+      </div>
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </nav>
   );
 };
